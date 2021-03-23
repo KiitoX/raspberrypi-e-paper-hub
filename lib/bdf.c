0e96e8e725a7e20f67f0f3e110afa8aa800d20c9
Emma <->
2021-03-23 14:43:49 +0100

    add bdf library

---
+++ lib/bdf.c
@@ -0,0 +1,263 @@
+//
+// Created by emma on 22/03/2021.
+//
+
+#include "bdf.h"
+
+enum token {
+    ASD
+};
+
+bool starts_with(const char *s, const char *prefix) {
+    return strncmp(s, prefix, strlen(prefix)) == 0;
+}
+
+void read_header(char *buffer, size_t buf_len, FILE *bdf_file, bdf_t *font) {
+    assert(starts_with(buffer, "STARTFONT 2.1"));
+
+    do {
+        assert(fgets(buffer, buf_len, bdf_file) != NULL);
+
+        if (starts_with(buffer, "SIZE")) {
+#ifdef DEBUG
+            printf("read: %s", buffer);
+#endif
+            assert(sscanf(buffer, "SIZE %hhu", &(font->size)) == 1);
+#ifdef DEBUG
+            printf("scan: size = %hhu\n\n", font->size);
+#endif
+        } else if (starts_with(buffer, "FONTBOUNDINGBOX")) {
+#ifdef DEBUG
+            printf("read: %s", buffer);
+#endif
+            assert(sscanf(buffer, "FONTBOUNDINGBOX %hhu %hhu %hhd %hhd",
+                          &(font->width), &(font->height),
+                          &(font->offsetX), &(font->offsetY)) == 4);
+#ifdef DEBUG
+            printf("scan: w = %hhu, h = %hhu, x = %hhd, y = %hhd\n\n",
+                   font->fontBounds.width, font->fontBounds.height,
+                   font->fontBounds.offsetX, font->fontBounds.offsetY);
+#endif
+        }
+    } while (!starts_with(buffer, "STARTPROPERTIES"));
+}
+
+void read_properties(char *buffer, size_t buf_len, FILE *bdf_file, bdf_t *font) {
+    do {
+        assert(fgets(buffer, buf_len, bdf_file) != NULL);
+
+        if (starts_with(buffer, "FONT_ASCENT")) {
+#ifdef DEBUG
+            printf("read: %s", buffer);
+#endif
+            assert(sscanf(buffer, "FONT_ASCENT %hhu", &(font->ascent)) == 1);
+#ifdef DEBUG
+            printf("scan: ascent = %hhu\n\n", font->ascent);
+#endif
+        } else if (starts_with(buffer, "FONT_DESCENT")) {
+#ifdef DEBUG
+            printf("read: %s", buffer);
+#endif
+            assert(sscanf(buffer, "FONT_DESCENT %hhu", &(font->descent)) == 1);
+#ifdef DEBUG
+            printf("scan: descent = %hhu\n\n", font->descent);
+#endif
+        } else if (starts_with(buffer, "DEFAULT_CHAR")) {
+#ifdef DEBUG
+            printf("read: %s", buffer);
+#endif
+            assert(sscanf(buffer, "DEFAULT_CHAR %hhu", &(font->defaultChar)) == 1);
+#ifdef DEBUG
+            printf("scan: default = %hhu\n\n", font->defaultChar);
+#endif
+        }
+    } while (!starts_with(buffer, "ENDPROPERTIES"));
+}
+
+void read_character(char *buffer, size_t buf_len, FILE *bdf_file, bdf_t *font, bitmap_t *bitmap) {
+    assert(fgets(buffer, buf_len, bdf_file) != NULL);
+    assert(starts_with(buffer, "STARTCHAR"));
+#ifdef BDF_CHARNAME
+#ifdef DEBUG
+    printf("read: %s", buffer);
+#endif
+    assert(sscanf(buffer, "STARTCHAR %ms", &bitmap->name) == 1);
+#ifdef DEBUG
+    printf("scan: charname = %s\n\n", bitmap->name);
+#endif
+#endif
+
+    do {
+        assert(fgets(buffer, buf_len, bdf_file) != NULL);
+
+        if (starts_with(buffer, "ENCODING")) {
+#ifdef DEBUG
+            printf("read: %s", buffer);
+#endif
+            assert(sscanf(buffer, "ENCODING %hu", &(bitmap->encoding)) == 1);
+#ifdef DEBUG
+            printf("scan: encoding = %hu\n\n", bitmap->encoding);
+#endif
+        } else if (starts_with(buffer, "DWIDTH")) {
+#ifdef DEBUG
+            printf("read: %s", buffer);
+#endif
+            assert(sscanf(buffer, "DWIDTH %hhu", &(bitmap->deviceWidth)) == 1);
+#ifdef DEBUG
+            printf("scan: width = %hhu\n\n", bitmap->deviceWidth);
+#endif
+        } else if (starts_with(buffer, "BBX")) {
+#ifdef DEBUG
+            printf("read: %s", buffer);
+#endif
+            assert(sscanf(buffer, "BBX %hhu %hhu %hhd %hhd",
+                          &(bitmap->width), &(bitmap->height),
+                          &(bitmap->offsetX), &(bitmap->offsetY)) == 4);
+#ifdef DEBUG
+            printf("scan: w = %hhu, h = %hhu, x = %hhd, y = %hhd\n\n",
+                   bitmap->charBounds.width, bitmap->charBounds.height,
+                   bitmap->charBounds.offsetX, bitmap->charBounds.offsetY);
+#endif
+        }
+    } while (!starts_with(buffer, "BITMAP"));
+
+    // round the width to the nearest multiple of eight required to fit it
+    size_t byte_width = ((bitmap->width + 7) / 8);
+    size_t required_bytes = byte_width * bitmap->height;
+    bitmap->bits = malloc(sizeof(*bitmap->bits) * required_bytes);
+    assert(bitmap->bits != NULL);
+
+    errno = 0;
+    for (int i = 0; i < required_bytes; ++i) {
+        assert(fgets(buffer, buf_len, bdf_file) != NULL);
+        bitmap->bits[i] = strtol(buffer, NULL, 16);
+        assert(errno == 0);
+    }
+
+    assert(fgets(buffer, buf_len, bdf_file) != NULL);
+    assert(starts_with(buffer, "ENDCHAR"));
+}
+
+bdf_t *bdf_read(const char *file_name, uint8_t scale) {
+    size_t buf_len = 250;
+    char *buffer = malloc(sizeof(*buffer) * buf_len);
+    assert(buffer != NULL);
+
+    FILE *bdf_file = fopen(file_name, "r");
+    assert(bdf_file != NULL);
+
+    assert(fgets(buffer, buf_len, bdf_file) != NULL);
+
+    bdf_t *font = malloc(sizeof(*font));
+    assert(font != NULL);
+    font->defaultChar = 0;
+    font->scale = scale;
+
+    read_header(buffer, buf_len, bdf_file, font);
+    read_properties(buffer, buf_len, bdf_file, font);
+
+    assert(fgets(buffer, buf_len, bdf_file) != NULL);
+#ifdef DEBUG
+    printf("read: %s", buffer);
+#endif
+    assert(starts_with(buffer, "CHARS"));
+    assert(sscanf(buffer, "CHARS %lu", &(font->numChars)) == 1);
+#ifdef DEBUG
+    printf("scan: chars = %lu\n\n", font->numChars);
+#endif
+
+    font->characters = malloc(sizeof(*font->characters) * font->numChars);
+    assert(font->characters != NULL);
+
+    for (int i = 0; i < font->numChars; ++i) {
+        read_character(buffer, buf_len, bdf_file, font, &font->characters[i]);
+    }
+
+    assert(fgets(buffer, buf_len, bdf_file) != NULL);
+    assert(starts_with(buffer, "ENDFONT"));
+
+    fclose(bdf_file);
+    free(buffer);
+
+    return font;
+}
+
+void bdf_free(bdf_t *font) {
+    for (int i = 0; i < font->numChars; ++i) {
+#ifdef BDF_CHARNAME
+        free(font->characters[i].name);
+#endif
+        free(font->characters[i].bits);
+    }
+
+    free(font->characters);
+    free(font);
+}
+
+size_t clamp(size_t val, size_t lower, size_t upper) {
+    return (val <= lower) ? lower : ((val > upper) ? upper : val);
+}
+
+int compare_encoding(const void *a, const void *b) {
+    return *(encoding_t *)a - ((bitmap_t *)b)->encoding;
+}
+
+// binary search
+bitmap_t *bdf_get_bitmap(bdf_t *font, encoding_t encoding) {
+    return bsearch(&encoding, font->characters, font->numChars, sizeof(*font->characters), compare_encoding);
+}
+
+// similar to interpolation search
+bitmap_t *bdf_get_bitmap_alt(bdf_t *font, encoding_t encoding) {
+    size_t i = 0, lower = 0, upper = font->numChars - 1, diff;
+    bitmap_t *selected;
+#ifdef DEBUG
+    size_t n = 0;
+#endif
+    do {
+        selected = &font->characters[i];
+        diff = encoding - selected->encoding;
+        if (diff >= 0) {
+            lower = i;
+        }
+        if (diff <= 0) {
+            upper = i;
+        }
+        i = clamp(i + diff, lower, upper);
+#ifdef DEBUG
+        n++;
+#endif
+    } while (lower < upper);
+#ifdef DEBUG
+    printf("selection iterations: %ld\n", n);
+#endif
+
+    if (selected->encoding == encoding) {
+        return selected;
+    } else {
+        return NULL;
+    }
+}
+
+void bdf_print_bitmap(bdf_t *font, bitmap_t *bitmap) {
+    size_t byte_width = ((bitmap->width + 7) / 8);
+
+    for (int y = 0; y < font->height * font->scale; ++y) {
+        for (int x = 0; x < font->width * font->scale; ++x) {
+            int bitmapX = (x / font->scale) + font->offsetX - bitmap->offsetX;
+            int bitmapY = (y / font->scale) - font->offsetY + bitmap->offsetY + bitmap->height - font->height;
+
+            if (0 <= bitmapX && bitmapX < bitmap->width &&
+                0 <= bitmapY && bitmapY < bitmap->height) {
+                if (((bitmap->bits[bitmapY * byte_width + (bitmapX / 8)] << (bitmapX % 8)) & 0x80) > 0) {
+                    printf("██");
+                } else {
+                    printf("▒▒");
+                }
+            } else {
+                printf("░░");
+            }
+        }
+        puts("");
+    }
+}
---
+++ lib/bdf.h
@@ -0,0 +1,58 @@
+//
+// Created by emma on 22/03/2021.
+//
+
+#ifndef EPAPER_BDF_H
+#define EPAPER_BDF_H
+
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <assert.h>
+#include <errno.h>
+
+// uint8_t is be enough if you only need ascii characters
+// uint32_t is required if you need specialised math, cjk, and/or emoji
+typedef uint16_t encoding_t;
+
+typedef struct {
+#ifdef BDF_CHARNAME
+    char *name; // limited to 14 chars in bdf 2.1
+#endif
+    uint8_t deviceWidth; // x offset for the next character
+    // bitmap bounds
+    uint8_t width; // in bits/pixels
+    uint8_t height; // in bits/pixels
+    // bitmap offset from origin
+    int8_t offsetX;
+    int8_t offsetY;
+    encoding_t encoding;
+    uint8_t *bits;
+} bitmap_t;
+
+typedef struct {
+    uint8_t size; // point size
+    uint8_t ascent; // pixels above the baseline
+    uint8_t descent; // pixels below the baseline, these two sum up to size
+    uint8_t defaultChar;
+    uint8_t scale;
+    // global character bounds
+    uint8_t width; // in bits/pixels
+    uint8_t height; // in bits/pixels
+    // origin offset from bottom left corner
+    int8_t offsetX;
+    int8_t offsetY;
+    size_t numChars;
+    bitmap_t *characters;
+} bdf_t;
+
+bdf_t *bdf_read(const char *file_name, uint8_t scale);
+void bdf_free(bdf_t *font);
+
+bitmap_t *bdf_get_bitmap(bdf_t *font, encoding_t encoding);
+
+void bdf_print_bitmap(bdf_t *font, bitmap_t *bitmap);
+
+#endif //EPAPER_BDF_H
